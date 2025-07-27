1b91603ed86588dc6cc5cf65983547db84b736c6
Devy4304 <148592924+Devy4304@users.noreply.github.com>
2025-07-27 10:14:46 -0700

    fix base name error

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/triviality-maker/',
+  base: '/Triviality-Maker/',
   build: {
     outDir: 'dist',
   },
